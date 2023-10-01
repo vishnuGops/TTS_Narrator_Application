2c7273350d77f53c80e3a157840c20bc9b01b5b6
Vishnu Gopal Rajan <vishvargops@gmail.com>
2023-09-30 23:08:05 -0700

    UI changes

--- TTS_app.py
+++ TTS_app.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import font
 import create_narration as Narration
 
 
@@ -8,19 +8,32 @@ def create_gui():
     root = tk.Tk()
     root.title("TTS Narration Application")
 
+    # Set the initial window size to 960x540
+    root.geometry("960x540")
+    root.configure(bg="white")
+
+    # Create a label for the heading title
+    heading_label = tk.Label(
+        root, text="TTS Narration Application", font=("Arial", 20))
+    heading_label.pack()
+
     # Create a text input field
-    input_text = tk.Text(root, height=10, width=40)
+    input_text = tk.Text(root, height=20, width=100)
     input_text.pack()
 
+    custom_font = font.Font(family="Arial", size=12, weight="bold")
     # Create a button to trigger narration creation
-    create_button = tk.Button(root, text="Create Narration", command=lambda:Narration.create_narration(input_text))
+    create_button = tk.Button(root, height=3, width=30, text="Create Narration", font=custom_font,
+                              bg="#9E9E9E", fg="white", command=lambda: Narration.create_narration(input_text))
     create_button.pack()
     return root
+
 
 def main():
     root = create_gui()
     # Run the main loop
     root.mainloop()
 
+
 if __name__ == "__main__":
-  main()
+    main()
